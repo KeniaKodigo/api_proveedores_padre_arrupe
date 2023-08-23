898f0b87d151f10589ad960008f738229ce81b47
KeniaKodigo <kenia.paz@kodigo.org>
2023-08-23 13:18:03 -0600

    nueva api

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+/**
+ * IMPORTANTE
+ * 
+ * LAS RUTAS PARA QUE FUNCIONEN SE AGREGAN EN API.PHP
+ * Y EN POSTMAN SE AGREGA API A LA RUTA ASIGNADA PARA QUE FUNCIONE
+ */
 
-Route::get('/proveedores', [ProveedoresController::class, 'index']);
+//Route::get('/proveedores', [ProveedoresController::class, 'index']);
 
 Route::post('/registrar', [ProveedoresController::class, 'store']);
 Route::put('/actualizar/{id}', [ProveedoresController::class, 'update']);
